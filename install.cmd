6fd743d190a83cad2cc69381af1138ba0d2ee5b6
atom.long <atom.long@hotmail.com>
2018-06-18 22:17:59 +0800

    Update CI script. Add VSTS CI script.

--- install.cmd
+++ install.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-if NOT EXIST "C:\msys%ARCH%-build" (
+if NOT EXIST "C:\msys%WIDTH%-build" (
 call :msys_install
 ) else (
 call :msys_init
@@ -10,24 +10,30 @@ call :msys_init
 exit /b %ERRORLEVEL%
 
 :msys_install
-if EXIST "C:\cygwin\bin" (
-call set "PATH=C:\cygwin\bin;%PATH%"
-) else if EXIST "C:\msys32\usr\bin" (
+if %CI_NAME%==drone (
 call set "PATH=C:\msys32\usr\bin;%PATH%"
-) else if EXIST "C:\msys64\usr\bin" (
-call set "PATH=C:\msys64\usr\bin;%PATH%"
+) else if %CI_NAME%==appveyor (
+call set "PATH=C:\cygwin\bin;%PATH%"
+) else if %CI_NAME%==VSTS (
+call set "PATH=C:\Program Files\Git\usr\bin;%PATH%"
 ) else (
-echo "No Cygwin or MSYS2 installed on your system."
+echo "Unsupported CI server %CI_NAME%"
 exit /b 1
 )
+
 curl -fsSL -o "%DISTRIB_FILE_NAME%" "%DISTRIB_FILE_URL%"
-bash --login -c "pushd C:/; mkdir msys%ARCH%-build; cd msys%ARCH%-build; tar --strip-components=1 -xf $(cygpath \"%CD%\%DISTRIB_FILE_NAME%\"); popd"
-C:\msys%ARCH%-build\usr\bin\bash --login -c "$(cygpath ${CI_BUILD_DIR})/ci-build.sh INSTALL"
-PATH=C:\msys%ARCH%-build\usr\bin;%PATH% && pacman --noconfirm --ask 20 --sync --refresh --refresh --sysupgrade --sysupgrade
-PATH=C:\msys%ARCH%-build\usr\bin;%PATH% && pacman --noconfirm --sync --needed base-devel msys2-devel git
-C:\msys%ARCH%-build\autorebase.bat
+if NOT EXIST %DISTRIB_FILE_NAME% (
+echo "Failed to Download MSYS2 distrib package."
+exit /b 2
+)
+
+bash --login -c "pushd C:/; mkdir msys%WIDTH%-build; cd msys%WIDTH%-build; tar --strip-components=1 -xf $(cygpath \"%CD%\%DISTRIB_FILE_NAME%\"); popd"
+C:\msys%WIDTH%-build\usr\bin\bash --login -c "$(cygpath ${CI_BUILD_DIR})/ci-build.sh INSTALL"
+PATH=C:\msys%WIDTH%-build\usr\bin;%PATH% && pacman --noconfirm --ask 20 --sync --refresh --refresh --sysupgrade --sysupgrade
+PATH=C:\msys%WIDTH%-build\usr\bin;%PATH% && pacman --noconfirm --sync --needed base-devel msys2-devel git
+call C:\msys%WIDTH%-build\autorebase.bat
 exit /b %ERRORLEVEL%
 
 :msys_init
-C:\msys%ARCH%-build\usr\bin\bash --login -c "$(cygpath ${CI_BUILD_DIR})/ci-build.sh INSTALL"
+C:\msys%WIDTH%-build\usr\bin\bash --login -c "$(cygpath ${CI_BUILD_DIR})/ci-build.sh INSTALL"
 exit /b %ERRORLEVEL%
